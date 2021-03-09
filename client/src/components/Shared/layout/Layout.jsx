3307294c6906b0259df4a294ccf8028ef79936f6
willwardlow <wardlow.william@gmail.com>
2021-03-09 14:14:20 -0600

    imported footer AND nav in layout

--- client/src/components/Shared/layout/Layout.jsx
+++ client/src/components/Shared/layout/Layout.jsx
@@ -1,10 +1,14 @@
 import Footer from '../footer/Footer.jsx'
+import Nav from '../Nav/Nav.jsx'
 
 import React from 'react';
 
 const Layout = (props) => {
   return (
     <div>
+      <div className='layout'>
+        <Nav user={user.props} />
+      </div>
       <div className='layout-children'>
         {props.children}
       </div>
