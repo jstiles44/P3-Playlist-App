234624ea806cc253e106b8b86465adad88ed8d50
willwardlow <wardlow.william@gmail.com>
2021-03-09 14:05:41 -0600

    Created Footer

---
+++ client/src/components/Shared/footer/Footer.jsx
@@ -0,0 +1,46 @@
+
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <div className='will'>
+        Will Wardlow
+         <a href='https://www.linkedin.com/in/will-wardlow/'>
+          <img classname='linked-in'src='https://www.iconsdb.com/icons/preview/color/7BAE7F/linkedin-4-xxl.png' alt='linkedin-page' />
+        </a>
+        <a href='https://github.com/willwardlow'>
+          <img className= 'github' src='https://www.iconsdb.com/icons/preview/color/7BAE7F/github-10-xxl.png' alt='github-page' />
+        </a>
+      </div>
+      <div className='lauren'>
+        Lauren Francis
+        <a href='https://www.linkedin.com/in/lauren-petto/'>
+          <img classname='linked-in'src='https://www.iconsdb.com/icons/preview/color/7BAE7F/linkedin-4-xxl.png' alt='linkedin-page' />
+        </a>
+        <a href='https://github.com/LaurenFrancisNYC'>
+          <img className= 'github' src='https://www.iconsdb.com/icons/preview/color/7BAE7F/github-10-xxl.png' alt='github-page' />
+        </a>
+      </div>
+      <div className='jake'>
+        Jake Stiles
+        <a href='https://www.linkedin.com/in/jacob-stiles-863b34152/'>
+          <img classname='linked-in'src='https://www.iconsdb.com/icons/preview/color/7BAE7F/linkedin-4-xxl.png' alt='linkedin-page' />
+        </a>
+        <a href='https://github.com/jstiles44'>
+          <img className= 'github' src='https://www.iconsdb.com/icons/preview/color/7BAE7F/github-10-xxl.png' alt='github-page' />
+        </a>
+      </div>
+      <div className='mike'>
+        Mike Pulido
+        <a href='https://www.linkedin.com/in/mpulido03/'>
+          <img classname='linked-in'src='https://www.iconsdb.com/icons/preview/color/7BAE7F/linkedin-4-xxl.png' alt='linkedin-page' />
+        </a>
+        <a href='https://github.com/mtpulido'>
+          <img className= 'github' src='https://www.iconsdb.com/icons/preview/color/7BAE7F/github-10-xxl.png' alt='github-page' />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
