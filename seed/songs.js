3400b8813dafc22639a04e7e4d5d837e035f931c
Jacob Stiles <jacobstiles@Jacobs-MacBook-Pro.local>
2021-03-09 12:18:45 -0500

    seeds added

--- seed/songs.js
+++ seed/songs.js
@@ -33,6 +33,33 @@ const main = async () => {
         "album": "Melophobia",
         "songLength": "3:29",
       },
+      {
+        "title": "Decks Dark",
+        "artist": "Radiohead",
+        "releaseYear": "2016",
+        "genre": "Alternative",
+        "albumCover": "http://images.genius.com/c9cf30826b50b5096664895e243253ad.1000x1000x1.jpg",
+        "album": "A Moon Shaped Pool",
+        "songLength": "4:41",
+        },
+    {
+        "title": "Ghost Ship",
+        "artist": "Blur,
+        "releaseYear": "2015",
+        "genre": "Alternative",
+        "albumCover": "https://images.genius.com/63bdd1821deba36e8ddaf164840dffd1.562x562x1.jpg",
+        "album": "The Magic Whip",
+        "songLength": "4:59",
+        },
+    {
+        "title": "Inertia Creeps",
+        "artist": "Massive Attack",
+        "releaseYear": "1998",
+        "genre": "Trip hop",
+        "albumCover": "https://www.udiscovermusic.com/wp-content/uploads/2019/04/Massive-Attack-Mezzanine-album-cover-web-optimised-820.jpg",
+        "album": "Mezzanine",
+        "songLength": "5:57",
+      },
      
     ]
   await Song.insertMany(songs)
