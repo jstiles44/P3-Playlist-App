8330425d4daa6168584cc9f4a022dceef31b46d9
willwardlow <wardlow.william@gmail.com>
2021-03-11 08:22:20 -0600

    updated icon links

--- client/src/components/Shared/footer/Footer.jsx
+++ client/src/components/Shared/footer/Footer.jsx
@@ -6,37 +6,37 @@ const Footer = () => {
       <div className='will'>
         Will Wardlow
          <a href='https://www.linkedin.com/in/will-wardlow/'>
-          <img className='linked-in'src='https://www.iconsdb.com/icons/preview/color/73D499/linkedin-4-xxl.png' alt='linkedin-page' />
+          <img className='linked-in'src='https://i.imgur.com/XU5XCFs.png' alt='linkedin-page' />
         </a>
         <a href='https://github.com/willwardlow'>
-          <img className= 'github' src='https://www.iconsdb.com/icons/preview/color/73D499/github-10-xxl.png' alt='github-page' />
+          <img className= 'github' src='https://i.imgur.com/3JoFkIB.png' alt='github-page' />
         </a>
       </div>
       <div className='lauren'>
         Lauren Francis
         <a href='https://www.linkedin.com/in/lauren-petto/'>
-          <img className='linked-in'src='https://www.iconsdb.com/icons/preview/color/73D499/linkedin-4-xxl.png' alt='linkedin-page' />
+          <img className='linked-in'src='https://i.imgur.com/XU5XCFs.png' alt='linkedin-page' />
         </a>
         <a href='https://github.com/LaurenFrancisNYC'>
-          <img className= 'github' src='https://www.iconsdb.com/icons/preview/color/73D499/github-10-xxl.png' alt='github-page' />
+          <img className= 'github' src='https://i.imgur.com/3JoFkIB.png' alt='github-page' />
         </a>
       </div>
       <div className='jake'>
         Jake Stiles
         <a href='https://www.linkedin.com/in/jacob-stiles-863b34152/'>
-          <img className='linked-in'src='https://www.iconsdb.com/icons/preview/color/73D499/linkedin-4-xxl.png' alt='linkedin-page' />
+          <img className='linked-in'src='https://i.imgur.com/XU5XCFs.png' alt='linkedin-page' />
         </a>
         <a href='https://github.com/jstiles44'>
-          <img className= 'github' src='https://www.iconsdb.com/icons/preview/color/73D499/github-10-xxl.png' alt='github-page' />
+          <img className= 'github' src='https://i.imgur.com/3JoFkIB.png' alt='github-page' />
         </a>
       </div>
       <div className='mike'>
         Mike Pulido
         <a href='https://www.linkedin.com/in/mpulido03/'>
-          <img className='linked-in' src='https://www.iconsdb.com/icons/preview/color/73D499/linkedin-4-xxl.png' alt='linkedin-page' />
+          <img className='linked-in' src='https://i.imgur.com/XU5XCFs.png' alt='linkedin-page' />
         </a>
         <a href='https://github.com/mtpulido'>
-          <img className= 'github' src='https://www.iconsdb.com/icons/preview/color/73D499/github-10-xxl.png' alt='github-page' />
+          <img className= 'github' src='https://i.imgur.com/3JoFkIB.png' alt='github-page' />
         </a>
       </div>
     </div>
