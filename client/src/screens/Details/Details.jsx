688c140b1918b9ce1212a798c27d053fce6e009c
Lauren Francis Petto <laurenpetto@gmail.com>
2021-03-11 13:19:49 -0500

    details make styled, added borders and scroll bar edit on profile, added borders on explore

--- client/src/screens/Details/Details.jsx
+++ client/src/screens/Details/Details.jsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react";
-import { useParams, Link, useHistory } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import { getSong } from "../../services/songs";
 import Layout from "../../components/Shared/layout/Layout";
-import {addSong} from "../../services/users"
+import { addSong } from "../../services/users";
+import "./Details.css";
 
 const Details = (props) => {
   const [song, setSong] = useState(null);
   const { id } = useParams();
   const [isLoaded, setLoaded] = useState(false);
-  
 
   useEffect(() => {
     const fetchSong = async () => {
@@ -23,30 +23,34 @@ const Details = (props) => {
     return <p>Loading...</p>;
   }
 
-  
-  
   const addToPlaylist = async () => {
-    const response = await addSong(props.user.id, song)
-    props.setUser(response)
-}
-
+    const response = await addSong(props.user.id, song);
+    props.setUser(response);
+  };
 
   return (
     <Layout user={props.user}>
       <div className="song-details">
         <img
-          className="album-art"
+          className="details-album-art"
           src={song.albumCover}
           alt={"album art for this song"}
         />
         <div className="detail">
-          <div className="name">{song.title}</div>
-          <div className="album">{song.album}</div>
-          <div className="genre">{song.genre}</div>
-          <div className="release-year">{song.releaseYear}</div>
-          <div className="song-length">{song.songLength}</div>
-          <button className="add-to-playlist" onClick={addToPlaylist}>
-              Add to Playlist 
+          <div classname="text">
+            <div className="title">{song.title}</div>
+            <div className="artist">by {song.artist}</div>
+            <div className="album">from the album {song.album}</div>
+            <div className="genre">Genre: {song.genre}</div>
+            <div className="release-year">Release Year: {song.releaseYear}</div>
+            <div className="song-length">Length: {song.songLength}</div>
+          </div>
+          <button
+            className="button_slide slide_right"
+            // className="add-to-playlist"
+            onClick={addToPlaylist}
+          >
+            Add to Playlist
           </button>
         </div>
       </div>
