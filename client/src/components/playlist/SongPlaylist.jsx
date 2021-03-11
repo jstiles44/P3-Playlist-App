d136c5c16d9ce4d95d7a0d3975f5f68a61bc9adb
Michael Pulido <michaelpulido@Michaels-MBP.maine.rr.com>
2021-03-10 22:18:37 -0500

    finished playlist styling

--- client/src/components/playlist/SongPlaylist.jsx
+++ client/src/components/playlist/SongPlaylist.jsx
@@ -1,11 +1,15 @@
-
+import "./SongPlaylist.css"
 
 
 const SongPlaylist = (props) => {
   return (
-    <div>
-      {props.title} {props.artist} {props.songLength}
-  </div>
+    <div className="playlist-song">
+      <div className="playlist-song-title">{props.title}</div>
+      <div className="playlist-song-artist">{props.artist} </div>
+      <div className="playlist-song-length">{props.songLength}</div>
+      <div className="playlist-song-rating">rating</div>
+      <div className="playlist-song-delete">X</div> 
+    </div>
   )
 }
 export default SongPlaylist
--- client/src/screens/Profile/Profile.jsx
+++ client/src/screens/Profile/Profile.jsx
@@ -1,8 +1,17 @@
 import Layout from "../../components/Shared/layout/Layout";
 import SongPlaylist from "../../components/playlist/SongPlaylist";
+import { useState, useEffect } from "react";
 import "./Profile.css";
 
 const Profile = (props) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   const { playlist } = props.user;
 
   const playlistSongsJSX = playlist.map((song) => (
@@ -16,17 +25,41 @@ const Profile = (props) => {
   return (
     <Layout user={props.user}>
       <div className="profile-container">
-
         <div className="playlist-container">
           <div className="playlist-labels">
-            Title: Artist: ⏱ Rating: Delete:
+            <div className="playlist-label-title">Title</div>{" "}
+            <div className="playlist-label-artist">Artist</div>{" "}
+            <div className="playlist-label-time">⏱</div>{" "}
+            <div className="playlist-label-rating">Rating</div>
+            <div className="playlist-label-delete">Delete</div>
           </div>
-
-          {playlistSongsJSX}
+          {loading ? (
+            <div className="loading-container">
+              <div className="loader"></div>
+            </div>
+          ) : (
+            <div>{playlistSongsJSX}</div>
+          )}
         </div>
 
-        <div className="favorites-profile">test</div>
-        <div className="analytics-profile">test</div>
+        <div className="favorites-profile">
+          {loading ? (
+            <div className="loading-container">
+              <div className="loader"></div>
+            </div>
+          ) : (
+            <div>test</div>
+          )}{" "}
+        </div>
+        <div className="analytics-profile">
+          {loading ? (
+            <div className="loading-container">
+              <div className="loader"></div>
+            </div>
+          ) : (
+            <div>test</div>
+          )}
+        </div>
       </div>
     </Layout>
   );
