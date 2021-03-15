e04e98ab1bc88ff1fe30d6450cafa13e2ad5dee7
Jacob Stiles <jacobstiles@Jacobs-MacBook-Pro.local>
2021-03-15 16:25:08 -0400

    css on rating fixed

--- client/src/components/ReviewForm/ReviewForm.jsx
+++ client/src/components/ReviewForm/ReviewForm.jsx
@@ -3,21 +3,30 @@ import './ReviewForm.css'
 
 const ReviewForm = ({rating, onChange, onSubmit}) => {
 
-
+ 
   return (
     <form className="review-create-form" onSubmit={onSubmit}>
-     
-              <select className="sort" onChange={(e) => onChange(e)}>
-                <option className="option" name="rating" value={1}>&nbsp; ⭐ &nbsp;</option>
-                <option className="option" name="rating" value={2}>&nbsp; ⭐️⭐️ &nbsp;</option>
-                <option className="option" name="rating" value={3}>&nbsp; ⭐️⭐️⭐️ &nbsp;</option>
-               <option className="option" name="rating" value={4}>&nbsp; ⭐️⭐️⭐️⭐️ &nbsp;</option>
-              <option className="option" name="rating" value={5}>&nbsp; ⭐️⭐️⭐️⭐️⭐️ &nbsp;</option>
-            </select>
-   
-      <button type='submit' className="review-submit-button">Submit</button>
+      <select className="rating-sort" onChange={(e) => onChange(e)}>
+        <option value="" disabled selected hidden>
+          Your Rating
+        </option>
+        <option className="option" name="rating" value={1}>&nbsp; ★ &nbsp;
+        </option>
+        <option className="option" name="rating" value={2}>&nbsp; ★★ &nbsp;
+        </option>
+        <option className="option" name="rating" value={3}>&nbsp; ★★★ &nbsp;
+        </option>
+        <option className="option" name="rating" value={4}>&nbsp; ★★★★ &nbsp;
+        </option>
+        <option className="option" name="rating" value={5}>&nbsp; ★★★★★ &nbsp;
+        </option>
+        </select>
+
+      <button type="submit" className="rating-submit">
+        Submit
+      </button>
     </form>
-  )
+  );
 }
 
 export default ReviewForm
